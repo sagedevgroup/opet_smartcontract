e85c1065b552db72720e19051152d1a90696cb31
Norfolk <norfolknorfolks@gmail.com>
2018-05-17 15:16:32 +0300

    Add tests for pause period for trasfers

--- test/test_opet_coin.js
+++ test/test_opet_coin.js
@@ -76,6 +76,16 @@ contract("Opet Token", async function(accounts) {
         assert.equal(afterAirdropBalance0.toNumber(), airdropAmounts[0], 'Wrong address balance after token airdrop for address 0');
         assert.equal(afterAirdropBalance1.toNumber(), airdropAmounts[1], 'Wrong address balance after token airdrop for address 1');
     });
+
+    it("test token set minter for airdrop", async function () {
+        let token = await Token.deployed();
+        let airdropAccounts = [accounts[3]];
+        let airdropAmounts = [1];
+        await token.setMinter(airdropAccounts[0], true);
+        await token.sendAirdrops(airdropAccounts, airdropAmounts, {'from': airdropAccounts[0]});
+        let afterAirdropBalance = BigNumber(await token.balanceOf(airdropAccounts[0]));
+        assert.equal(afterAirdropBalance.toNumber(), 1, 'Wrong address balance after token mint');
+    });
 });
 
 contract("Opet Token", async function(accounts) {
@@ -96,4 +106,40 @@ contract("Opet Token", async function(accounts) {
             assert.equal(err, 'Error: VM Exception while processing transaction: revert', "Wrong error too big amount of airdrop");
         }
     });
+});
+
+
+contract("Opet Token", async function (accounts) {
+    it("test transfer unable to execute", async function () {
+        let token = await Token.deployed();
+        let mintDestination = accounts[1];
+        let transferDestination = accounts[2];
+
+
+        await token.mint(mintDestination, 2);
+        try {
+            await token.transfer(transferDestination, 1, {'from': mintDestination});
+            assert.ifError('Error, transfer should be unable');
+        } catch (err) {
+            assert.equal(err, 'Error: VM Exception while processing transaction: revert', "Transfer works before token unpaused");
+        }
+
+        await token.approve(transferDestination, 1, {'from': mintDestination});
+        try {
+            await token.transferFrom(mintDestination, transferDestination, 1, {'from': transferDestination});
+            assert.ifError('Error, transferFrom should be unable');
+        } catch (err) {
+            assert.equal(err, 'Error: VM Exception while processing transaction: revert', "transferFrom works before token unpaused");
+        }
+
+        await token.unpauseTransfer();
+        await token.transfer(transferDestination, 1, {'from': mintDestination});
+        let afterTransferBalance = BigNumber(await token.balanceOf(transferDestination));
+        assert.equal(afterTransferBalance.toNumber(), 1, 'Wrong address balance after transfer');
+
+        await token.transferFrom(mintDestination, transferDestination, 1, {'from': transferDestination});
+        afterTransferBalance = BigNumber(await token.balanceOf(transferDestination));
+        assert.equal(afterTransferBalance.toNumber(), 2, 'Wrong address balance after transferFrom');
+    })
+
 });
